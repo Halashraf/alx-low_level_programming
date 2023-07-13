e18e02ebb59f11b04e047a1854acf624d4ac48af
Halashraf <halaashrafsaleh@gmail.com>
2023-07-13 15:39:04 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
- */
+ * Description: A C program that print with printf function 
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-        puts("\"with proper grammar, but the outcome is a piece of art,\n");
+        printf("\"with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
